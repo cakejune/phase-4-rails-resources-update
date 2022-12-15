a48c3a5831bdc9771fe745772db674ee8718e7ae
cakejune <tyq444@gmail.com>
2022-12-15 11:18:41 -0500

    commit tree 0967ef4bba4ab558ad678c2ff38bed944495ce2f

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,18 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+
+
+  def update
+    bird = Bird.find(params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
+
   # POST /birds
   def create
     bird = Bird.create(bird_params)
@@ -22,10 +34,20 @@ class BirdsController < ApplicationController
     end
   end
 
+  def increment_likes
+    bird = Bird.find(params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
